d9d8a4b03e1aa0272d12a128ceceab194a9bc92c
shubidumdu <skaty4870@naver.com>
2023-05-15 16:01:19 +0900

    [WIP] save export feature

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,50 @@ const JSDOS_OPTIONS: DosPlayerOptions = {
 	style: 'hidden',
 };
 
-const WRAPPER = document.getElementById('jsdos');
+const wrapper = document.getElementById('jsdos');
+const gamePath = require('./hwanse.jsdos');
+let dosInterface: any;
 
 // @ts-ignore
 emulators.pathPrefix = 'js-dos/';
 // @ts-ignore
-Dos(WRAPPER, JSDOS_OPTIONS).run(require('./hwanse.jsdos'));
+Dos(wrapper, JSDOS_OPTIONS)
+	.run(gamePath)
+	.then((ci: any) => {
+		dosInterface = ci;
+	});
+
+const exportSave = async () => {
+	// const openRequest = indexedDB.open('js-dos-cache (emulators-ui-saves)', 1);
+	// openRequest.onsuccess = async () => {
+	// 	let db = openRequest.result;
+	// 	const transaction = db.transaction('files', 'readonly');
+	// 	const fileStore = transaction.objectStore('files');
+	// 	const files = fileStore.getAll();
+	// 	files.onsuccess = async () => {
+	// 		const [saveBufferArray] = files.result;
+	// 		const stringified = JSON.stringify([...new Int8Array(saveBufferArray)]);
+	// 		await navigator.clipboard.writeText(stringified);
+	// 		alert('클립보드에 저장되었습니다.');
+	// 		db.close();
+	// 	};
+	// };
+	// console.log(ci);
+	if (dosInterface) {
+		const changesBundle = await dosInterface.persist();
+		await navigator.clipboard.writeText(changesBundle);
+		alert('클립보드에 저장되었습니다.');
+	}
+};
+
+const importSave = async () => {
+	const bufferText = prompt('세이브 텍스트를 입력해주세요.');
+	try {
+		const saveBufferURL = URL.createObjectURL(new Blob([bufferText]));
+		// @ts-ignore
+		// await Dos(wrapper, JSDOS_OPTIONS).run(gamePath, );
+	} catch (err) {}
+};
+
+document.getElementById('export-save').addEventListener('click', exportSave);
+document.getElementById('import-save').addEventListener('click', importSave);
