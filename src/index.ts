24a2cded58c626f87b2a18b5535e084ea2906a3d
shubidumdu <skaty4870@naver.com>
2023-05-16 22:46:34 +0900

    fix: 키 입력 문제 완화

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { DosPlayerOptions } from '../node_modules/js-dos/dist/types/src/player';
 
 const JSDOS_OPTIONS: DosPlayerOptions = {
 	style: 'hidden',
+	preventUnload: true,
 };
 
 const wrapper = document.getElementById('jsdos');
@@ -13,29 +14,21 @@ let dosInterface: any;
 
 // @ts-ignore
 emulators.pathPrefix = 'js-dos/';
+
 // @ts-ignore
 Dos(wrapper, JSDOS_OPTIONS)
 	.run(gamePath)
 	.then((ci: any) => {
 		dosInterface = ci;
+		ci.sendKeyEvent = (key: number, type: boolean) => {
+			ci.addKey(key, type, Date.now() - ci.startedAt);
+			requestAnimationFrame(() => {
+				ci.addKey(key, false, Date.now() - ci.startedAt);
+			});
+		};
 	});
 
 const exportSave = async () => {
-	// const openRequest = indexedDB.open('js-dos-cache (emulators-ui-saves)', 1);
-	// openRequest.onsuccess = async () => {
-	// 	let db = openRequest.result;
-	// 	const transaction = db.transaction('files', 'readonly');
-	// 	const fileStore = transaction.objectStore('files');
-	// 	const files = fileStore.getAll();
-	// 	files.onsuccess = async () => {
-	// 		const [saveBufferArray] = files.result;
-	// 		const stringified = JSON.stringify([...new Int8Array(saveBufferArray)]);
-	// 		await navigator.clipboard.writeText(stringified);
-	// 		alert('클립보드에 저장되었습니다.');
-	// 		db.close();
-	// 	};
-	// };
-	// console.log(ci);
 	if (dosInterface) {
 		const changesBundle = await dosInterface.persist();
 		await navigator.clipboard.writeText(changesBundle);
@@ -46,11 +39,21 @@ const exportSave = async () => {
 const importSave = async () => {
 	const bufferText = prompt('세이브 텍스트를 입력해주세요.');
 	try {
-		const saveBufferURL = URL.createObjectURL(new Blob([bufferText]));
+		const saveBufferURL = URL.createObjectURL(
+			new Blob([new Uint8Array(JSON.parse(`[${bufferText}]`)).buffer]),
+		);
+		wrapper.innerHTML = '';
 		// @ts-ignore
-		// await Dos(wrapper, JSDOS_OPTIONS).run(gamePath, );
+		await Dos(wrapper, JSDOS_OPTIONS).run(gamePath, saveBufferURL);
 	} catch (err) {}
 };
 
 document.getElementById('export-save').addEventListener('click', exportSave);
 document.getElementById('import-save').addEventListener('click', importSave);
+
+// ArrowUp = 265;
+// ArrowDown = 264;
+// ArrowLeft = 263;
+// ArrowRight = 262;
+// Enter = 257;
+// Escape = 256;
